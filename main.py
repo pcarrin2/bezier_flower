4720a503c62c8ef94811f2b05cafc7cb12832895
Pomona Carrington Hoekstra <pcarrin2@illinois.edu>
2025-08-20 14:33:57 -0500

    finish the bounding box thing for real for real

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cairo
 import math
 import random
+from sys import argv
 
 # aww hell yeah we got globals :(
 radius = 150
 margin = 200
-num_points = 3
+num_points = int(argv[1])
 
-ctrl_radius = radius * math.sin(2 * math.pi/num_points)
+ctrl_radius = radius * math.sin(2*math.pi/num_points)
 side_length = 2*(radius+margin)
 
 # determine whether points p1 and p2 are on the same side of the segment defined by
@@ -18,39 +19,45 @@ def same_side(s1, s2, p1, p2):
 
 def draw_annotations(context, points, ctrl_pts):
     # draw circle in grey
-   #context.set_source_rgb(0.5, 0.5, 0.5)
-   #context.set_line_width(2)
-   #context.set_line_join(cairo.LineJoin.ROUND)
-   #context.arc(*center, radius, 0, 2*math.pi)
-   #context.stroke()
+    context.set_source_rgb(0.5, 0.5, 0.5)
+    context.set_line_width(2)
+    context.set_line_join(cairo.LineJoin.ROUND)
+    context.arc(*center, radius, 0, 2*math.pi)
+    context.stroke()
 
     # draw points and control radii in red
-   #context.set_source_rgb(1, 0, 0)
+    context.set_source_rgb(1, 0, 0)
 
-   #for p in points:
-   #    context.arc(*p, 5, 0, 2*math.pi)
-   #    context.fill()
-   #    context.arc(*p, ctrl_radius, 0, 2*math.pi)
-   #    context.stroke()
+    for p in points:
+        context.arc(*p, 5, 0, 2*math.pi)
+        context.fill()
+        context.arc(*p, ctrl_radius, 0, 2*math.pi)
+        context.stroke()
 
     # draw control points in green/blue
-   #green = True
-   #for p in ctrl_pts:
-   #    if green:
-   #        context.set_source_rgb(0,1,0)
-   #        green = False
-   #    else:
-   #        context.set_source_rgb(0,0,1)
-   #        green = True
-   #    context.arc(*p, 5, 0, 2*math.pi)
-   #    context.fill()
+    green = True
+    for p in ctrl_pts:
+        if green:
+            context.set_source_rgb(0,1,0)
+            green = False
+        else:
+            context.set_source_rgb(0,0,1)
+            green = True
+        context.arc(*p, 5, 0, 2*math.pi)
+        context.fill()
 
     # draw bounding boxes in transparent grey
     for i in range(num_points):
         if same_side(ctrl_pts[2*i], ctrl_pts[2*i+1], points[i-1], points[i]):
-            hull_points = [points[i-1], ctrl_pts[2*i], ctrl_pts[2*i+1], points[i]] 
+            if same_side(points[i-1], ctrl_pts[2*i], points[i], ctrl_pts[2*i+1]):
+                hull_points = [points[i-1], ctrl_pts[2*i], ctrl_pts[2*i+1], points[i]] 
+            else:
+                hull_points = [points[i-1], ctrl_pts[2*i+1], ctrl_pts[2*i], points[i]] 
         else:
-            hull_points = [points[i-1], ctrl_pts[2*i], points[i], ctrl_pts[2*i+1]] 
+            if same_side(points[i-1], ctrl_pts[2*i+1], points[i], ctrl_pts[2*i]):
+                hull_points = [points[i-1], ctrl_pts[2*i+1], points[i], ctrl_pts[2*i]] 
+            else:
+                hull_points = [points[i-1], ctrl_pts[2*i], points[i], ctrl_pts[2*i+1]] 
 
         context.set_source_rgba(0,0,0,0.2)
         context.move_to(*hull_points[-1])
