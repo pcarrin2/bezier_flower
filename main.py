e96fa7a70bd7a48c2e24c9a01ea2a9ba578b9f61
Pomona Carrington Hoekstra <pcarrin2@illinois.edu>
2025-08-20 17:39:34 -0500

    spaghetti code rasterization

--- main.py
+++ main.py
@@ -2,25 +2,28 @@ import cairo
 import math
 import random
 from sys import argv
+from draw_bezier import draw_curve
+from PIL import Image
 
 # aww hell yeah we got globals :(
-radius_min = 0
-radius_max = 150
-steps = 100
+radius_min = 100
+radius_max = 300
+steps = 200
 
 radii = [radius_max - i * (radius_max-radius_min)/steps for i in range(steps)] 
 
-margin = 200
+margin = 100
 num_points = int(argv[1])
 side_length = 2*(radius_max+margin)
 center = (radius_max+margin, radius_max+margin)
 
-ctrl_angles_start = [i*(2*math.pi/num_points) - math.pi*random.random()/2 for i in range(num_points)]
-ctrl_angles_finish = [i*(2*math.pi/num_points) - math.pi*random.random()/2 for i in range(num_points)]
+#ctrl_angles_start = [i*(2*math.pi/num_points) - math.pi*random.random()/2 for i in range(num_points)]
+ctrl_angles_start = [i*(2*math.pi/num_points) - 2*math.pi*random.random() for i in range(num_points)]
+#ctrl_angles_finish = [i*(2*math.pi/num_points) - math.pi*random.random()/2 for i in range(num_points)]
+ctrl_angles_finish = [i*(2*math.pi/num_points) - math.pi/2 for i in range(num_points)]
 ctrl_angles_steps = []
 for i in range(steps):
     ctrl_angles_steps.append([ctrl_angles_start[j] + i*(ctrl_angles_finish[j]-ctrl_angles_start[j])/steps for j in range(num_points)])
-
 
 
 def ctrl_radius(radius):
@@ -32,88 +35,27 @@ def ctrl_radius(radius):
 def same_side(s1, s2, p1, p2):
     return ((s1[1]-s2[1])*(p1[0]-s1[0])+(s2[0]-s1[0])*(p1[1]-s1[1]))*((s1[1]-s2[1])*(p2[0]-s1[0])+(s2[0]-s1[0])*(p2[1]-s1[1]))>0
 
-def draw_annotations(context, points, ctrl_pts, radius, center):
-    # draw circle in grey
-    context.set_source_rgb(0.5, 0.5, 0.5)
-    context.set_line_width(2)
-    context.set_line_join(cairo.LineJoin.ROUND)
-    context.arc(*center, radius, 0, 2*math.pi)
-    context.stroke()
+im = Image.new(mode="RGB", size=(side_length, side_length))
+px = im.load()
 
-    # draw points and control radii in red
-    context.set_source_rgb(1, 0, 0)
-
-    for p in points:
-        context.arc(*p, 5, 0, 2*math.pi)
-        context.fill()
-        context.arc(*p, ctrl_radius(radius), 0, 2*math.pi)
-        context.stroke()
-
-    # draw control points in green/blue
-    green = True
-    for p in ctrl_pts:
-        if green:
-            context.set_source_rgb(0,1,0)
-            green = False
-        else:
-            context.set_source_rgb(0,0,1)
-            green = True
-        context.arc(*p, 5, 0, 2*math.pi)
-        context.fill()
-
-    # draw bounding boxes in transparent grey
+for step in range(steps):
+    radius = radii[step] 
+    ctrl_angles = ctrl_angles_steps[step]
+    # points along big circle
+    points = [(center[0] + radius*math.cos(i*(2*math.pi/num_points)), center[1] + radius*math.sin(i*(2*math.pi/num_points))) for i in range(num_points)]
+    
+    ctrl_pts = []
     for i in range(num_points):
-        if same_side(ctrl_pts[2*i], ctrl_pts[2*i+1], points[i-1], points[i]):
-            if same_side(points[i-1], ctrl_pts[2*i], points[i], ctrl_pts[2*i+1]):
-                hull_points = [points[i-1], ctrl_pts[2*i], ctrl_pts[2*i+1], points[i]] 
-            else:
-                hull_points = [points[i-1], ctrl_pts[2*i+1], ctrl_pts[2*i], points[i]] 
-        else:
-            if same_side(points[i-1], ctrl_pts[2*i+1], points[i], ctrl_pts[2*i]):
-                hull_points = [points[i-1], ctrl_pts[2*i+1], points[i], ctrl_pts[2*i]] 
-            else:
-                hull_points = [points[i-1], ctrl_pts[2*i], points[i], ctrl_pts[2*i+1]] 
+        ctrl_pts.append(
+                (points[i][0] + ctrl_radius(radius)*math.cos(ctrl_angles[i]), 
+                 points[i][1] + ctrl_radius(radius)*math.sin(ctrl_angles[i])))
+        ctrl_pts.append(
+                (points[i][0] - ctrl_radius(radius)*math.cos(ctrl_angles[i]),
+                 points[i][1] - ctrl_radius(radius)*math.sin(ctrl_angles[i])))
 
-        context.set_source_rgba(0,0,0,0.2)
-        context.move_to(*hull_points[-1])
-        for p in hull_points:
-            context.line_to(*p)
-        context.fill()
+    ctrl_pts.append(ctrl_pts.pop(0))
+    # Drawing Curve
+    for i in range(num_points):
+        draw_curve(px, points[i-1], points[i], [ctrl_pts[2*i], ctrl_pts[2*i+1]], 0, 20, 50)
 
-# creating a SVG surface
-with cairo.SVGSurface("out.svg", side_length, side_length) as surface:
-    context = cairo.Context(surface)
-    for step in range(steps):
-        radius = radii[step] 
-        ctrl_angles = ctrl_angles_steps[step]
-        # points along big circle
-        points = [(center[0] + radius*math.cos(i*(2*math.pi/num_points)), center[1] + radius*math.sin(i*(2*math.pi/num_points))) for i in range(num_points)]
-        
-        ctrl_pts = []
-        for i in range(num_points):
-            ctrl_pts.append(
-                    (points[i][0] + ctrl_radius(radius)*math.cos(ctrl_angles[i]), 
-                     points[i][1] + ctrl_radius(radius)*math.sin(ctrl_angles[i])))
-            ctrl_pts.append(
-                    (points[i][0] - ctrl_radius(radius)*math.cos(ctrl_angles[i]),
-                     points[i][1] - ctrl_radius(radius)*math.sin(ctrl_angles[i])))
-
-        context.set_source_rgba(0,0,0,0.2)
-        context.set_line_width(1)
-        context.set_line_join(cairo.LineJoin.ROUND)
-        
-        context.move_to(*points[0])
-        points.append(points.pop(0))
-        ctrl_pts.append(ctrl_pts.pop(0))
-        # Drawing Curve
-        for i in range(num_points):
-            context.curve_to(*ctrl_pts[2*i], *ctrl_pts[2*i+1], *points[i])
-        context.stroke()
-
-        #draw_annotations(context, points, ctrl_pts, radius, center)
-
-
-
-# printing message when file is saved
-print("File Saved")
-
+im.save("out.png")
